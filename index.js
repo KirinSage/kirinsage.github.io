4b4086f01d885c4f3374c90b70b23440cb94a37b
KirinSage <kiirin025@gmail.com>
2024-12-31 18:50:28 +0000

    version 0.0.5 tab implementation

--- index.js
+++ index.js
@@ -2,13 +2,19 @@
 
 let aboutTab = document.querySelector("#about");
 let linksTab = document.querySelector("#links");
+let aboutContent = document.querySelector("#about_content");
+let linksContent = document.querySelector("#links_content")
 
 aboutTab.addEventListener('click', function(){
     aboutTab.classList = "active_tab";
     linksTab.classList = "inactive_tab";
+    aboutContent.classList = "open";
+    linksContent.classList = "closed";
 })
 
 linksTab.addEventListener('click', function(){
     aboutTab.classList = "inactive_tab";
     linksTab.classList = "active_tab";
+    aboutContent.classList = "closed";
+    linksContent.classList = "oepn";
 })
